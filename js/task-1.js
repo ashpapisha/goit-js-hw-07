945b3306d775fa5b88078a9702e1999bd7b9d2f7
se <stasyaroshyk@gmail.com>
2026-09-25 20:38:43 +0300

    2

--- js/task-1.js
+++ js/task-1.js
@@ -3,7 +3,7 @@ const categoryItems = categoriesList.querySelectorAll(".item");
 
 console.log(`Number of categories: ${categoryItems.length}`);
 
-categoryItems.forEach((item) => {
+categoryItems.forEach(item => {
   const categoryName = item.querySelector("h2").textContent;
   const elementsCount = item.querySelectorAll("ul li").length;
   console.log(`Category: ${categoryName}`);
